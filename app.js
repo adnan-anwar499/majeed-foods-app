a83027e5c6db693d6881ae1d83c0abd093724e43
adnan-anwar499 <adnan.anwer13@gmail.com>
2026-03-20 22:53:00 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-UPDATED APP FILE
+function showTab(tabName){
+  document.getElementById("breakdown").style.display = "none";
+  document.getElementById("pm").style.display = "none";
+  document.getElementById("equipment").style.display = "none";
+  document.getElementById("spares").style.display = "none";
+
+  document.getElementById(tabName).style.display = "block";
+}
